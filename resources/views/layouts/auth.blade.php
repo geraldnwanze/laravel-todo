790747550dd8db3fbfa5c2f06ebd808f7bfe4e0a
geraldon1997 <geraldon1997@gmail.com>
2023-04-03 20:34:49 +0100

    testing headway app

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -50,6 +50,8 @@ input:-webkit-autofill {
 
 @yield('content')
 
+<div class="head"></div>
+
 <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-gray-900 border-t border-gray-200 shadow text-center md:p-6 dark:bg-gray-900 dark:border-gray-600">
   <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
     © 2023 <a href="#" class="hover:underline">{{ config('app.name') }}</a>. All Rights Reserved.
@@ -61,5 +63,14 @@ input:-webkit-autofill {
 </form>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
+
+<script>
+  // @see https://docs.headwayapp.co/widget for more configuration options.
+  var HW_config = {
+    selector: ".head", // CSS selector where to inject the badge
+    account:  "7qQgW7"
+  }
+</script>
+<script async src="https://cdn.headwayapp.co/widget.js"></script>
 </body>
 </html>
